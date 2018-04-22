97a9d2905f69a60cd97f1dab00d7ddacb1de399b
Manuel Ruck <github@manuelruck.de>
2018-04-22 14:24:19 +0200

    add map region to map component

--- OverpassApp/src/screens/Map/index.js
+++ OverpassApp/src/screens/Map/index.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { View, Text } from "react-native";
 import MapView from "react-native-maps";
 
-const Map = () => {
-  return (
-    <View>
-      <Text>The Map</Text>
-      <MapView
-        style={{ width: "100%", height: "100%" }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
-    </View>
-  );
-};
+class Map extends Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+  };
+
+  onRegionChange = region => {
+    this.setState({ region });
+  };
+
+  render() {
+    return (
+      <View>
+        <MapView
+          style={{ width: "100%", height: "100%" }}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        />
+      </View>
+    );
+  }
+}
 
 export default Map;
