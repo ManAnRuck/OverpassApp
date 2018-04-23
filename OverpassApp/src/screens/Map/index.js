9c7b4bd1fcb8a59a8f447de3f676e6d08e0334c2
Manuel Ruck <github@manuelruck.de>
2018-04-23 07:53:27 +0200

    show map position

--- OverpassApp/src/screens/Map/index.js
+++ OverpassApp/src/screens/Map/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 import { View, Text, Platform } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 import styled from "styled-components";
 import { Icon, FormInput } from "react-native-elements";
+
+import getCurrentLocation from "../../utils/getCurrentLocation";
 
 const Wrapper = styled.View`
   flex: 1;
@@ -12,13 +14,26 @@ const Wrapper = styled.View`
 
 const MenuButton = styled(Icon).attrs({
   raised: true,
-  name: "heartbeat",
+  name: "refresh",
   type: "font-awesome",
   color: "#f50",
   containerStyle: {
     position: "absolute",
     right: 18,
     top: Platform.OS === "ios" ? 24 : 18
+  }
+})``;
+
+const Locate = styled(Icon).attrs({
+  raised: true,
+  name: "location-arrow",
+  type: "font-awesome",
+  color: "blue",
+  size: 18,
+  containerStyle: {
+    position: "absolute",
+    left: 18,
+    bottom: 18
   }
 })``;
 
@@ -36,33 +51,71 @@ const OverpassQuery = styled(Icon).attrs({
 
 class Map extends Component {
   state = {
-    region: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
-    },
-    codeVisible: true,
-    code: `
-
-[out:json][timeout:3600];
-// gather results
-
-  // query part for: “"Drinking Water"”
-//  node["amenity"="drinking_water"]({{bbox}});
-//area;
-//area[name="ประเทศไทย"]->.boundaryarea;
-//node["amenity"="drinking_water"](area.boundaryarea);
-
+    codeVisible: false,
+    code: `[out:json][timeout:3600];
 node({{bbox}})["amenity"="drinking_water"];
+out;`,
+    markers: [],
+    isLoading: false
+  };
 
-// print results
-out;
-    `
+  region: {
+    latitude: 13.75326914242899,
+    longitude: 100.55545276402489,
+    latitudeDelta: 0.5709253825609935,
+    longitudeDelta: 0.514984130859375
+  };
+
+  onMapReady = () => {
+    return getCurrentLocation().then(position => {
+      if (position) {
+        this.map.animateToRegion({
+          ...position.coords,
+          latitudeDelta: 0.0025384406541064664,
+          longitudeDelta: 0.0024686381220817566
+        });
+      }
+    });
   };
 
   onRegionChange = region => {
-    this.setState({ region });
+    this.region = region;
+  };
+
+  loadData = async () => {
+    this.setState({
+      isLoading: true
+    });
+    const { region } = this;
+
+    const bbox = `${region.latitude - region.latitudeDelta},${region.longitude -
+      region.longitudeDelta},${region.latitude +
+      region.latitudeDelta},${region.longitude + region.longitudeDelta}`;
+
+    const data = await fetch(
+      `https://lz4.overpass-api.de/api/interpreter?[out:json][timeout:3600];node(${bbox})[amenity=drinking_water];out;`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(responseJson => {
+        this.setState({
+          markers: responseJson.elements.map(marker => ({
+            id: marker.id,
+            coordinate: {
+              latitude: marker.lat,
+              longitude: marker.lon
+            }
+          }))
+        });
+        this.setState({
+          isLoading: false
+        });
+        return responseJson.elements;
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   render() {
@@ -70,21 +123,33 @@ out;
       <Wrapper>
         <MapView
           style={{ flex: 1 }}
-          // region={this.state.region}
-          // onRegionChange={this.onRegionChange}
-          initialRegion={this.state.region}
-        />
+          provider="google"
+          ref={ref => {
+            this.map = ref;
+          }}
+          showsUserLocation
+          onRegionChangeComplete={this.onRegionChange}
+          onMapReady={this.onMapReady}
+        >
+          {this.state.markers.map(({ id, coordinate }) => (
+            <Marker key={id} coordinate={coordinate} />
+          ))}
+        </MapView>
         <MenuButton
-          onPress={() =>
-            this.setState({
-              codeVisible: !this.state.codeVisible
-            })
-          }
+          onPress={() => this.loadData()}
+          reverse={this.state.isLoading}
         />
-        {this.state.codeVisible && (
-          <OverpassQuery onPress={() => console.log("hello")} />
-        )}
         {!this.state.codeVisible && (
+          <OverpassQuery
+            onPress={() => {
+              Overpass(this.state.code, (error, data) => {
+                console.log(error, data);
+              });
+            }}
+          />
+        )}
+        <Locate />
+        {this.state.codeVisible && (
           <FormInput multiline={true} value={this.state.code} />
         )}
       </Wrapper>
---
+++ OverpassApp/src/utils/getCurrentLocation.js
@@ -0,0 +1,8 @@
+export default () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position => resolve(position),
+      e => reject(e)
+    );
+  });
+};
